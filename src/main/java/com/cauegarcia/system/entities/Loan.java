3803290d9425ed84fa5bc4954f916ae47f69a1fa
Caue Garcia <cauerg7@outlook.com>
2023-12-23 11:17:18 -0300

    Loan entity, plugin maven, LoanSatatus - enum

---
+++ src/main/java/com/cauegarcia/system/entities/Loan.java
@@ -0,0 +1,59 @@
+package com.cauegarcia.system.entities;
+
+import com.cauegarcia.system.entities.enums.LoanStatus;
+
+import java.time.Instant;
+import java.util.Objects;
+
+public class Loan {
+
+    private Long id;
+    private Instant moment;
+    private LoanStatus status;
+
+    public Loan(){
+    }
+
+    public Loan(Long id, Instant moment, LoanStatus status) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public LoanStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(LoanStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Loan loan = (Loan) o;
+        return Objects.equals(id, loan.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/cauegarcia/system/entities/enums/LoanStatus.java
@@ -0,0 +1,6 @@
+package com.cauegarcia.system.entities.enums;
+
+public enum LoanStatus {
+
+    LOANED, PAID, WAITING;
+}
