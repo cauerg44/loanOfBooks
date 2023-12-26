b59327a6dbe396596f0742f06a053a2f6f497e31
Caue Garcia <cauerg7@outlook.com>
2023-12-25 22:51:21 -0300

    ResponseEntity to endpoints

--- src/main/java/com/cauegarcia/system/controllers/BooksController.java
+++ src/main/java/com/cauegarcia/system/controllers/BooksController.java
@@ -1,11 +1,16 @@
 package com.cauegarcia.system.controllers;
 
+import com.cauegarcia.system.entities.Book;
 import com.cauegarcia.system.entities.dto.BookDTO;
 import com.cauegarcia.system.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/books")
@@ -15,18 +20,22 @@ public class BooksController {
     private BookService service;
 
     @GetMapping(value = "/{id}")
-    public BookDTO findById(@PathVariable Long id) {
+    public ResponseEntity<BookDTO> findById(@PathVariable Long id) {
         BookDTO bookDTO = service.findById(id);
-        return bookDTO;
+        return ResponseEntity.ok(bookDTO);
     }
 
     @GetMapping
-    public Page<BookDTO> findAllBooks(Pageable pageable) {
-        return service.findAllBooks(pageable);
+    public ResponseEntity<Page<BookDTO>> findAllBooks(Pageable pageable) {
+        Page<BookDTO> bookDTO = service.findAllBooks(pageable);
+        return ResponseEntity.ok(bookDTO);
     }
 
     @PostMapping
-    public BookDTO insert(@RequestBody BookDTO bookDTO) {
-        return service.insert(bookDTO);
+    public ResponseEntity<BookDTO> insert(@RequestBody BookDTO bookDTO) {
+        bookDTO = service.insert(bookDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(bookDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(bookDTO);
     }
 }
