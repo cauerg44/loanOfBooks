35ddc6502897f08211d829a1c97362344c92da4a
Caue Garcia <cauerg7@outlook.com>
2023-12-27 10:28:39 -0300

    user details, grantedAutority, not testes

--- src/main/java/com/cauegarcia/system/entities/Role.java
+++ src/main/java/com/cauegarcia/system/entities/Role.java
@@ -1,12 +1,13 @@
 package com.cauegarcia.system.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Objects;
 
 @Entity
 @Table(name = "roles")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,6 +31,7 @@ public class Role {
         this.id = id;
     }
 
+    @Override
     public String getAuthority() {
         return authority;
     }
